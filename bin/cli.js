581f5fdaff98c5a1f47e258ee5b0664644061dec
David Fundter <david.fundter@gmail.com>
2019-08-16 22:44:10 +0200

    Added ability to show cycle by date to CLI

--- bin/cli.js
+++ bin/cli.js
@@ -4,19 +4,24 @@ const airac = require("../dist/index");
 const airacInfo = require("../package.json")
 const yargs = require("yargs");
 
+const isoDatePattern = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
+
 const args = yargs
+    .usage("Usage: $0 [-i <airac-identifier>|-d <iso-date>]")
     .alias("i", "identifier")
-    .describe("i", "AIRAC identifier to use (e.g. 1910)")
+    .describe("i", "AIRAC identifier to use (e.g. '1909')")
     .string("i")
     .alias("d", "date")
-    .describe("d", "Date in ISO format to use (e.g. 2019-08-15)")
+    .describe("d", "Date in ISO format to use (e.g. '2019-08-15')")
     .string("d")
+    .conflicts("identifier", "date")
     .help("h")
     .alias("h", "help")
     .example("$0", "Prints the AIRAC cycle for the current date.")
-    .example("$0 -i <cycle-identifier>", "Prints the AIRAC cycle identified by the given identifier")
+    .example("$0 -i <airac-identifier>", "Prints the AIRAC cycle identified by the given AIRAC identifier")
     .example("$0 -d <iso-date>", "Prints the AIRAC cycle for the given date")
-    .version(`airac v${airacInfo.version}`)
+    .version("version", "Show version", `airac v${airacInfo.version}`)
+    .alias("v", "version")
     .locale("en")
     .argv;
 
@@ -24,15 +29,22 @@ if (args.identifier) {
     try {
         const cycle = airac.Cycle.fromIdentifier(args.identifier);
         cycle.printInfo();
-    }
-    catch (error) {
+    } catch (error) {
         if (error instanceof airac.InvalidCycleIdentifierError) {
             console.error(error.message);
+            process.exit(1);
         } else {
             throw error;
         }
     }
+} else if (args.date) {
+    if (!args.date.match(isoDatePattern)) {
+        console.error(`Not a valid ISO date string: '${args.date}'`);
+        process.exit(1);       
+    }
+    const cycle = airac.Cycle.fromDate(new Date(args.date));
+    cycle.printInfo();
+} else {
+    const cycle = airac.Cycle.fromDate(new Date());
+    cycle.printInfo();    
 }
-
-//const cycle = airac.Cycle.fromDate(new Date());
-//cycle.printInfo();
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,5 @@
-import * as yargs from "yargs";
 import { Cycle } from "./airac";
 import { InvalidCycleIdentifierError } from "./errors";
-
-if (require.main === module) {
-    const args = yargs
-        .usage("Usage: ")
-        .argv;
-
-    try {
-        const cycle = args._.length > 0 ?
-            Cycle.fromIdentifier(String(args._[0])) :
-            Cycle.fromDate(new Date());
-        console.log(`AIRAC cycle: ${cycle.identifier}`);
-        console.log(`Effective from (inc.): ${cycle.effectiveStart.toLocaleDateString()}`);
-        console.log(`Effective to (inc.): ${cycle.effectiveEnd.toLocaleDateString()}`);
-    } catch (error) {
-        if (error instanceof InvalidCycleIdentifierError) {
-            console.error(error.message);
-        } else {
-            throw error;
-        }
-    }
-}
 
 export {
     Cycle,
