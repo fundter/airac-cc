144fb0a78afeb01070ad3d2e942cf160c03571f0
David Fundter <david.fundter@gmail.com>
2019-08-16 20:50:22 +0200

    Added yargs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
+import * as yargs from "yargs";
 import { Cycle } from "./airac";
 import { InvalidCycleIdentifierError } from "./errors";
 
 if (require.main === module) {
+    const args = yargs
+        .usage("Usage: ")
+        .argv;
+
     try {
-        const cycle = process.argv.length < 3 ?
-            Cycle.fromDate(new Date()) :
-            Cycle.fromIdentifier(process.argv[2]);
+        const cycle = args._.length > 0 ?
+            Cycle.fromIdentifier(String(args._[0])) :
+            Cycle.fromDate(new Date());
         console.log(`AIRAC cycle: ${cycle.identifier}`);
-        console.log(`Effective from (inc.): ${cycle.effectiveStart.toLocaleDateString("de")}`);
-        console.log(`Effective to (inc.): ${cycle.effectiveEnd.toLocaleDateString("de")}`);
+        console.log(`Effective from (inc.): ${cycle.effectiveStart.toLocaleDateString()}`);
+        console.log(`Effective to (inc.): ${cycle.effectiveEnd.toLocaleDateString()}`);
     } catch (error) {
         if (error instanceof InvalidCycleIdentifierError) {
             console.error(error.message);
